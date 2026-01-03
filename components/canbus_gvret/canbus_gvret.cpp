8571b26fa363dc11629be4304c5950817dacf812
hencou <32905287+hencou@users.noreply.github.com>
2026-01-03 11:45:19 +0100

    Update canbus_gvret.cpp

--- components/canbus_gvret/canbus_gvret.cpp
+++ components/canbus_gvret/canbus_gvret.cpp
@@ -28,8 +28,8 @@ void CanbusGVRET::setup() {
 
   wifiManager.setup();
 
-  settings.useBinarySerialComm = true;
-  settings.systemType = 1;
+  //settings.useBinarySerialComm = true;
+  //settings.systemType = 1;
 
   this->is_initialized = true;
   ESP_LOGCONFIG(TAG, "setup done");
@@ -145,6 +145,7 @@ void CanbusGVRET::loop() {
 
 }  // namespace canbus_gvret
 }  // namespace esphome
+
 
 
 
